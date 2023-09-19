e596780ff8f4ff2bb0178423eb4b0700badf4f10
yavor <jm87585@gmail.com>
2023-09-19 20:51:27 +0300

    Create Home Page.

---
+++ src/main/java/com/springinaction/tacos/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.springinaction.tacos.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/test/java/com/springinaction/tacos/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springinaction.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
